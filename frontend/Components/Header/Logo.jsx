1ed40955a6cc0aec2b825520ab3e2a9f366de1d6
Narek-Momchyan <narekmomchyan80@gmail.com>
2026-06-18 20:58:37 +0400

    feat: Calibrate odds and configure dynamic audios

--- frontend/Components/Header/Logo.jsx
+++ frontend/Components/Header/Logo.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import style from './header.module.css'
+import Link from 'next/link'
   
 export default function Logo({logo}) {
   return (
     <div className={style.logoWrapper}>
-      <img src={logo?.img_route} alt={logo?.logo_route} />
+      <Link href="/"><img src={logo?.img_route} alt={logo?.logo_route} /></Link>
+      
     </div>
   )
 }
